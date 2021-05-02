892908d85bc48cba64db462636c6044c54b617cf
asepboy <asepsr58@gmail.com>
2021-05-02 00:58:22 +0000

    ansible dumbways

---
+++ ansible.sh
@@ -0,0 +1,5 @@
+sudo apt -y update && sudo apt -y upgrade
+sudo apt-get -y install python3
+sudo apt install whois -y
+sudo apt-add-repository --update --yes ppa:ansible/ansible
+sudo apt -y install ansible
